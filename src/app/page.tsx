e23e13e2ea8a935694692dfa2d499b9436143473
Thang Nguyen <thang.dev.ptit@gmail.com.com>
2024-03-08 16:36:12 +0700

    Init project

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from './components/Header';
+import RestaurantCard from './components/RestaurantCard';
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-start">
+        <RestaurantCard />
+      </div>
+    </main>
+  );
+}
